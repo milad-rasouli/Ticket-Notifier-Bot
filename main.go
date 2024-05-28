ed02948418c1183c82d5c85ab03cca0973110eb8
Milad Rasouli <milad75rasouli@gmail.com>
2024-05-28 20:59:23 +0330

    chore: ignore bin & network_mode

--- main.go
+++ main.go
@@ -1,7 +1,25 @@
 package main
 
-import "log"
+import (
+	"fmt"
+
+	"github.com/gocolly/colly/v2"
+)
 
 func main() {
-	log.Println("Hi")
+	c := colly.NewCollector()
+
+	// Find and visit all links
+	c.OnHTML("a[href]", func(e *colly.HTMLElement) {
+
+		fmt.Println("OnHTML ", e.Attr("href"))
+		fmt.Println("OnHTML name: ", e.Name)
+		//e.Request.Visit(e.Attr("href"))
+	})
+
+	c.OnRequest(func(r *colly.Request) {
+		fmt.Println("Visiting", r.URL)
+	})
+
+	c.Visit("https://safar724.com/bus/mashhad-gonabad?date=1403-03-08")
 }
